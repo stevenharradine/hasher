0ef3192ad69bc3123735f021fa2ee124503eadf4
Steven Harradine <stevenharradine@gmail.com>
2022-02-19 16:51:54 -0500

    fix tests

--- tests/create-sha1.sh
+++ tests/create-sha1.sh
@@ -7,8 +7,6 @@ results=`bash hasher.sh --mode=create --directory=tests/workspace`
 # Evaluate the test case
 if [ "`cat tests/workspace/file1.avi.sha1`" == "38be7d1b981f2fb6a4a0a052453f887373dc1fe8" ]; then
 	echo "Pass"
-	((++number_of_tests_passed))
 else
 	echo "Fail"
-	((++number_of_tests_failed))
 fi
--- tests/create-sha256.sh
+++ tests/create-sha256.sh
@@ -7,8 +7,6 @@ results=`bash hasher.sh --mode=create --directory=tests/workspace`
 # Evaluate the test case
 if [ "`cat tests/workspace/file1.avi.sha256`" == "ecdc5536f73bdae8816f0ea40726ef5e9b810d914493075903bb90623d97b1d8" ]; then
 	echo "Pass"
-	((++number_of_tests_passed))
 else
 	echo "Fail"
-	((++number_of_tests_failed))
 fi
